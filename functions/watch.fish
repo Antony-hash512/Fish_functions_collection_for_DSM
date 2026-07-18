0800bcb365082b3731eaa22b0dce66630c61ff4e
Antony <antony.ictr@gmail.com>
2026-07-19 03:05:00 +0400

    feat: add watch function for repeated command execution in Fish

--- functions/watch.fish
+++ functions/watch.fish
@@ -1,12 +1,27 @@
-function watch --description 'Repeatedly run a command every 2 seconds'
+function watch --description 'Repeatedly run a command'
+    argparse 'n/interval=' -- $argv
+    or return
+
+    if not set -q argv[1]
+        echo "watch: missing command" >&2
+        return 1
+    end
+
+    set -l interval 2
+    if set -q _flag_interval
+        set interval $_flag_interval
+    end
+
     while true
         clear
         set_color yellow
         echo "Custom watch loop (fish). Press Ctrl+C to stop."
         echo "Command: $argv"
+        echo "Interval: $interval seconds"
         set_color normal
         echo "------------------------------------------"
         eval $argv
-        sleep 2
+        sleep $interval
     end
 end
+
